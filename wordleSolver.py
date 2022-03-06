6f38f679faf39fc0e67177c20e55ad4118b4bf99
Leonardo Saveri <75999407+leonardosaveri@users.noreply.github.com>
2022-03-06 17:37:50 +0100

    Main script
    The main python script

--- wordleSolver.py
+++ wordleSolver.py
@@ -10,11 +10,16 @@ notIn = []
 inside = dict()
 c = 1
 while c:
-    best = get_best(wordList)
-    print(f"We suggest trying with: {best}")
-    if input("Found the solution? (y/n)\n").lower() == "y":
+    print(f"We suggest trying with: {get_best(wordList)}")
+    choice = input("What word do you choose:\n")
+    best = choice if choice in wordList else get_best(wordList)
+    guess = input("Input the solution, "
+                  "use uppercase for letters in position, "
+                  "lowercase for letters included but not in place "
+                  "and \'-\' for empty (Example: L-a-i)\n")
+    if guess.lower() == best.lower():
         break
-    ans, notIn, inside = ask_correct(ans, notIn, inside, best)
+    ans, notIn, inside = ask_correct(guess, ans, notIn, inside, best)
     wordList = remove_from_letters(wordList, notIn)
     wordList = only_with_green(wordList, ans)
     wordList = only_with_yellow(wordList, inside)
